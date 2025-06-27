4dd27634b093acd40c50e7756cf6ecf79d4d78d9
Ruslan Sakovich <rufil1.rs@gmail.com>
2025-06-27 16:51:41 +0300

    feat: final end part2

--- 2 ПЕРМЕННЫЕ, ОПЕРАТОРЫ/2.6 конвертер миль в километры/index.js
+++ 2 ПЕРМЕННЫЕ, ОПЕРАТОРЫ/2.6 конвертер миль в километры/index.js
@@ -1,3 +1,3 @@
 let milles = 130;
-let kilometers = (milles * 0.621);
-console.log("'расстояние'  milles  'миль.'' равно' + kilometers + 'км'" );
+let kilometers = milles / 0.621;
+console.log('расстояние ' + milles + ' миль.' + ' равно ' + Math.floor(kilometers) + ' км.');
