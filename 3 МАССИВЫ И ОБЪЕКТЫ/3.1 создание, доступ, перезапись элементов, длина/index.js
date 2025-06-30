19ff49d85209f312b0281ffa76a925adee423179
Ruslan Sakovich <rufil1.rs@gmail.com>
2025-06-30 16:50:22 +0300

    feat: added example 3.1

---
+++ 3 МАССИВЫ И ОБЪЕКТЫ/3.1 создание, доступ, перезапись элементов, длина/index.js
@@ -0,0 +1,16 @@
+
+byList = ["Milk", "Bread", "Appels"];
+
+console.log(byList[1]); // доступ к элементу массива
+
+console.log(byList.length); //длина массива
+
+
+byList[1] = "Bananas"; //замена элемента массива
+console.log(byList);
+
+console.log(byList[byList.length - 1]); //доступ к последнему элементу массива
+
+
+
+
