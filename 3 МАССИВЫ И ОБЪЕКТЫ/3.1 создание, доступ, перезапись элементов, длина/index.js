904b50354e0a10a5c7d1876061a2a67f7d1ebb48
Ruslan Sakovich <rufil1.rs@gmail.com>
2025-07-01 16:44:11 +0300

    feat: added part3

--- 3 МАССИВЫ И ОБЪЕКТЫ/3.1 создание, доступ, перезапись элементов, длина/index.js
+++ 3 МАССИВЫ И ОБЪЕКТЫ/3.1 создание, доступ, перезапись элементов, длина/index.js
@@ -6,7 +6,7 @@ console.log(byList[1]); // доступ к элементу массива
 console.log(byList.length); //длина массива
 
 
-byList[1] = "Bananas"; //замена элемента массива
+byList[0] = "Bananas"; //замена элемента массива
 console.log(byList);
 
 console.log(byList[byList.length - 1]); //доступ к последнему элементу массива
---
+++ 3 МАССИВЫ И ОБЪЕКТЫ/3.2 добавление, удаление элементов, поиск, сортировка, reverse/index.js
@@ -0,0 +1,13 @@
+
+
+favoriteFruits = ["grapefruit", "orange", "lemon"]; //созданный массив
+  console.log(favoriteFruits);
+
+favoriteFruits.push("tangerine"); //добавление элемента массива
+  console.log(favoriteFruits);
+  console.log(favoriteFruits.length);
+
+let lenghtOfFavoriteFruits = favoriteFruits.push("lime"); //добавление элемента массива, с сохранинием нового массива в переменной
+  console.log(favoriteFruits);  
+
+  
